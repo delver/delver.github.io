f764ab518faed8f1e6fa7cf8780b782085af65df
Thom Lawrence <thom@delver.io>
2014-09-30 12:20:23 +0100

    Experimenting with package format.

---
+++ packages/thom-clojure-1.el
@@ -0,0 +1,16 @@
+;;; thom-clojure.el --- Thom's clojure config
+;; Copyright (C) 2014 Thom Lawrence
+
+;; Author: Thom Lawrence <thom@delver.io>
+;; Maintainer: Thom Lawrence <thom@delver.io>
+;; URL: http://github.com/delver/delver.github.io/packages/thom-clojure-1.el
+;; Created: 30th September 2014
+;; Version: 1
+;; Keywords: lisp
+;; Package-Requires: ((cider (0 6 0)(paredit (22))
+
+;;; Code:
+
+(add-hook 'clojure-mode-hook 'paredit-mode)
+
+;;; thom-clojure.el ends here
