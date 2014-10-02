15ca97d16bf51ac04d4b2f8f0cca886234fae7fb
Thom Lawrence <thom@delver.io>
2014-10-02 23:31:59 +0100

    Playing with fringes and margins.

--- packages/thom-3.el
+++ packages/thom-3.el
@@ -5,7 +5,7 @@
 ;; Maintainer: Thom Lawrence <thom@delver.io>
 ;; URL: http://github.com/delver/delver.github.io/packages/thom.el
 ;; Created: 30th September 2014
-;; Version: 2
+;; Version: 3
 ;; Keywords: lisp
 ;; Package-Requires: ((thom-ido "1")(thom-mac "1")(thom-text "1")(thom-clojure "3"))
 
@@ -17,8 +17,12 @@
   (setq backup-inhibited t)
   (setq auto-save-default nil)
   (global-linum-mode 1)
+  (setq linum-format "%d ")
   (column-number-mode t)
   (setq-default truncate-lines t)
+  (set-frame-parameter nil 'right-fringe 5)
+  (set-frame-parameter nil 'left-fringe 0)
+  (setq indicate-empty-lines t)
   (menu-bar-mode -1)
   (scroll-bar-mode -1)
   (tool-bar-mode -1))
