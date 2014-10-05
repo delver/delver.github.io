bb369878bad8ef959c7ec6a344d54c347f431d3e
Thom Lawrence <thom@delver.io>
2014-10-05 21:31:37 +0100

    Only update file if running cider.

--- packages/thom-clojure.el
+++ packages/thom-clojure.el
@@ -5,7 +5,7 @@
 ;; Maintainer: Thom Lawrence <thom@delver.io>
 ;; URL: http://github.com/delver/delver.github.io/packages/thom-clojure.el
 ;; Created: 30th September 2014
-;; Version: 7
+;; Version: 8
 ;; Keywords: lisp
 ;; Package-Requires: ((cider "0.7.0")(paredit "22")(rainbow-blocks "0.1"))
 
@@ -18,7 +18,13 @@
   (add-hook 'clojure-mode-hook 'paredit-mode)
   (add-hook 'clojure-mode-hook 'rainbow-blocks-mode)
   (add-hook 'clojure-mode-hook 'show-paren-mode)
-  (add-hook 'clojure-mode-hook (lambda () (add-hook 'after-save-hook 'cider-load-current-buffer nil t)))
+  (add-hook 'clojure-mode-hook
+	    (lambda ()
+	      (add-hook 'after-save-hook 
+			(lambda ()
+			  (when (bound-and-true-p 'cider-mode)
+			    (cider-load-current-buffer)))
+			nil t)))
   (add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
   (setq nrepl-hide-special-buffers t)
   (setq cider-repl-use-clojure-font-lock t)
