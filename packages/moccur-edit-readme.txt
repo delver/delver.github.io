b5792b37d748ebbbe4670920f4317b6674dde74b
Thom Lawrence <thom@delver.io>
2014-12-02 11:46:57 +0000

    Added color-occur and moccur-edit.

---
+++ packages/moccur-edit-readme.txt
@@ -0,0 +1,9 @@
+;;; Commentary:
+
+;; moccur-edit provides to edit moccur buffer of color-moccur.el and
+;; to apply the changes to the file.
+
+;; This file requires color-moccur.el
+;; The latest version of these program can be downloaded from
+;; http://www.bookshelf.jp/elc/color-moccur.el.
+
---
+++ packages/thom-occur.el
@@ -0,0 +1,23 @@
+;;; thom-occur.el --- Thom's occur config
+;; Copyright (C) 2014 Thom Lawrence
+
+;; Author: Thom Lawrence <thom@delver.io>
+;; Maintainer: Thom Lawrence <thom@delver.io>
+;; URL: http://github.com/delver/delver.github.io/packages/thom-occur.el
+;; Created: 2nd December 2014
+;; Version: 2
+;; Keywords: lisp
+;; Package-Requires: (("color-moccur" "2.71")("moccur-edit" "2.16"))
+
+;;; Code:
+
+;;;###autoload
+(progn
+  (require 'color-moccur)
+  (require 'moccur-edit)
+
+  (defadvice moccur-edit-change-file
+    (after save-after-moccur-edit-buffer activate)
+    (save-buffer))
+
+;;; thom-occur.el ends here
